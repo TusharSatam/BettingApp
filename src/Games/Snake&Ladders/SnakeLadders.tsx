1caa9f1ab64a38ba5739119a311bacb265c4f630
TusharSatam <tsatam91@gmail.com>
2024-03-22 19:11:01 +0530

    added dice ui

--- src/Games/Snake&Ladders/SnakeLadders.tsx
+++ src/Games/Snake&Ladders/SnakeLadders.tsx
@@ -7,30 +7,71 @@ import {
   Image,
   Animated,
   Easing,
+  TouchableOpacity,
 } from 'react-native';
-
+import FastImage from 'react-native-fast-image';
 const SnakeLadders: React.FC = () => {
   const [playerPositions, setPlayerPositions] = useState<number[]>([1, 1]);
   const [currentPlayerIndex, setCurrentPlayerIndex] = useState<number>(0);
-  const [diceValue, setDiceValue] = useState<number>(0);
+  const [dice1Value, setDice1Value] = useState<number>(6);
+  const [dice2Value, setDice2Value] = useState<number>(6);
+  const [isDice1Rolling, setisDice1Rolling] = useState(false);
+  const [isDice2Rolling, setisDice2Rolling] = useState(false);
+
   const [winner, setWinner] = useState<number | null>(null);
   const playerColors = ['green', 'red'];
-
+  const playerNames = ['Player 1', 'Player 2'];
+  const diceSide = [
+    require('./assets/images/dice/dice1.jpeg'),
+    require('./assets/images/dice/dice2.jpeg'),
+    require('./assets/images/dice/dice3.jpeg'),
+    require('./assets/images/dice/dice4.jpeg'),
+    require('./assets/images/dice/dice5.jpeg'),
+    require('./assets/images/dice/dice6.jpeg'),
+  ];
   // Animation state for player movement
   const playerAnimations = playerPositions.map(
     position => new Animated.Value(0),
   );
 
-  // Function to handle dice roll
+  const handleDiceRolling = (user: String) => {
+    if (user === 'green') {
+      setisDice1Rolling(true);
+    } else {
+      setisDice2Rolling(true);
+    }
+    setTimeout(() => {
+      rollDice();
+    }, 1000);
+  };
+
   // Function to handle dice roll
   const rollDice = () => {
     if (winner !== null) return; // Don't roll dice if winner is declared
     const diceValue = Math.floor(Math.random() * 6) + 1;
+    console.log(
+      `Player ${currentPlayerIndex === 0 ? 'Tushar' : 'Max'}:`,
+      diceValue,
+    );
+
     if (currentPlayerIndex === 0) {
-      setDiceValue(diceValue);
+      setisDice1Rolling(false);
+      setDice1Value(diceValue);
+    }
+    if (currentPlayerIndex === 1) {
+      setisDice2Rolling(false);
+      setDice2Value(diceValue);
     }
     const currentPosition = playerPositions[currentPlayerIndex];
     const remainingSteps = 100 - currentPosition;
+
+    // Check if the player is at position 1 and rolled a number other than 6
+    if (currentPosition === 1 && diceValue !== 6) {
+      // If the player is at position 1 and did not roll a 6, they cannot move
+      // Move to the next player's turn
+      setCurrentPlayerIndex((currentPlayerIndex + 1) % playerPositions.length);
+      return;
+    }
 
     // Check if the player is standing on position 96 and rolled a number greater than the remaining steps
     if (currentPosition >= 95 && diceValue > remainingSteps) {
@@ -52,12 +93,15 @@ const SnakeLadders: React.FC = () => {
         newPosition = 100;
       }
 
+      // Apply ladder logic
+      const originalPosition = newPosition;
+      newPosition = checkForLadderSnake(newPosition);
+
       const updatedPositions = [...playerPositions];
-      updatedPositions[currentPlayerIndex] = checkForLadderSnake(newPosition);
-      animatePlayerMovement(
-        currentPlayerIndex,
-        updatedPositions[currentPlayerIndex],
-      );
+      updatedPositions[currentPlayerIndex] = newPosition;
+
+      // Animate player movement
+      animatePlayerMovement(currentPlayerIndex, newPosition);
       setPlayerPositions(updatedPositions);
 
       // Check if any player has reached position 100
@@ -69,6 +113,12 @@ const SnakeLadders: React.FC = () => {
         // Declare the winning player
         setWinner(winningPlayerIndex);
       } else {
+        // Check if the player rolled a 6 or climbed a ladder
+        if (diceValue === 6 || newPosition > originalPosition) {
+          // If the player rolled a 6 or climbed a ladder, give them another chance
+          return;
+        }
+        // Move to the next player's turn
         setCurrentPlayerIndex(
           (currentPlayerIndex + 1) % playerPositions.length,
         );
@@ -108,7 +158,7 @@ const SnakeLadders: React.FC = () => {
   const animatePlayerMovement = (playerIndex: number, newPosition: number) => {
     const currentPosition = playerPositions[playerIndex];
     const steps = Math.abs(newPosition - currentPosition); // Calculate number of steps
-    const durationPerStep = 2000; // Duration for each step in milliseconds
+    const durationPerStep = 200; // Duration for each step in milliseconds
     let animatedValue = currentPosition;
 
     for (let i = 0; i <= steps; i++) {
@@ -121,29 +171,18 @@ const SnakeLadders: React.FC = () => {
 
       // Update animatedValue for next step
       animatedValue += newPosition > currentPosition ? 1 : -1;
-
-      // Delay the next step to ensure sequential movement
-      setTimeout(() => {}, i * durationPerStep);
     }
   };
-
-  // Effect to automatically roll dice for the second player after the first player
-  useEffect(() => {
-    if (currentPlayerIndex === 1 && winner === null) {
-      const delay = setTimeout(() => {
-        rollDice();
-      }, 1000);
-      return () => clearTimeout(delay);
-    }
-  }, [currentPlayerIndex, winner]);
 
   // Function to reset the game
   const resetGame = () => {
     setPlayerPositions([1, 1]);
     setCurrentPlayerIndex(0);
-    setDiceValue(0);
+    setDice1Value(0);
+    setDice2Value(0);
     setWinner(null);
   };
+
   // Generate numbers from 100 to 1, with every second row reversed
   const generateNumbers = (): number[] => {
     const rows = [];
@@ -194,8 +233,8 @@ const SnakeLadders: React.FC = () => {
                           backgroundColor: playerColors[playerIndex],
                           left: '20%',
                           bottom: '50%',
-                          borderWidth:2,
-                          borderColor:"white",
+                          borderWidth: 2,
+                          borderColor: 'white',
                           transform: [
                             {
                               translateY: playerAnimations[
@@ -218,13 +257,97 @@ const SnakeLadders: React.FC = () => {
           {/* Render player tokens */}
         </View>
       </View>
-      <Text style={{color: 'white'}}>
-        Player 1 Position: {playerPositions[0]} Dice:{diceValue}
-      </Text>
+      <Text style={{color: 'white'}}>Tushar Dice:{dice1Value}</Text>
+      <Text style={{color: 'white'}}>Max Dice: {dice2Value}</Text>
       {winner !== null && (
-        <Text style={{color: 'white'}}>Player {winner + 1} wins!</Text>
+        <Text style={{color: 'white'}}>{playerNames[winner]} wins!</Text>
       )}
-      {winner === null && <Button title="Roll Dice" onPress={rollDice} />}
+      <View
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          width: '100%',
+          paddingHorizontal: 20,
+        }}>
+        {winner === null && (
+          <View style={{display: 'flex', flexDirection: 'row'}}>
+            <View
+              style={[
+                {
+                  height: 16,
+                  width: 16,
+                  backgroundColor: playerColors[0],
+                  borderWidth: 2,
+                  borderColor: 'white',
+                  borderRadius: 4,
+                },
+              ]}
+            />
+            <TouchableOpacity
+              onPress={() => handleDiceRolling('green')}
+              disabled={currentPlayerIndex !== 0 && !isDice2Rolling}
+              style={{
+                borderWidth: 1,
+                borderColor: currentPlayerIndex === 0 ? 'black' : 'transparent',
+                borderRadius: 4,
+                opacity: currentPlayerIndex === 0 && !isDice2Rolling ? 1 : 0.3,
+              }}>
+              {!isDice1Rolling ? (
+                <Image
+                  source={diceSide[dice1Value - 1]}
+                  style={{height: 34, width: 34}}
+                />
+              ) : (
+                <FastImage
+                  resizeMode="contain"
+                  style={{width: 34, height: 34}}
+                  source={require('./assets/images/dice/Dice.gif')}
+                />
+              )}
+            </TouchableOpacity>
+          </View>
+        )}
+
+        {winner === null && (
+          <View style={{display: 'flex', flexDirection: 'row'}}>
+            <View
+              style={[
+                {
+                  height: 16,
+                  width: 16,
+                  backgroundColor: playerColors[1],
+                  borderWidth: 2,
+                  borderColor: 'white',
+                  borderRadius: 4,
+                },
+              ]}></View>
+            <TouchableOpacity
+              onPress={() => handleDiceRolling('red')}
+              disabled={currentPlayerIndex !== 1 && !isDice1Rolling}
+              style={{
+                borderWidth: 1,
+                borderColor: currentPlayerIndex === 1 ? 'black' : 'transparent',
+                borderRadius: 4,
+                opacity: currentPlayerIndex === 1 && !isDice1Rolling ? 1 : 0.3,
+              }}>
+              {!isDice2Rolling ? (
+                <Image
+                  source={diceSide[dice2Value - 1]}
+                  style={{height: 34, width: 34}}
+                />
+              ) : (
+                <FastImage
+                  resizeMode="contain"
+                  style={{width: 34, height: 34}}
+                  source={require('./assets/images/dice/Dice.gif')}
+                />
+              )}
+            </TouchableOpacity>
+          </View>
+        )}
+      </View>
+
       {winner !== null && <Button title="Reset Game" onPress={resetGame} />}
     </View>
   );
@@ -241,7 +364,6 @@ const styles = StyleSheet.create({
     gap: 10,
     color: 'black',
   },
-
   board: {
     padding: 0,
     display: 'flex',
